c16d8938735addf32eb5cea0af058a1ae27c2803
francoisLF <francois.lefort_pro@yahoo.com>
2018-03-12 15:57:58 +0100

    Breakpoints initialisation

---
+++ app/src/main/java/com/example/francoislf/moodtracker/Controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.francoislf.moodtracker.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.francoislf.moodtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
