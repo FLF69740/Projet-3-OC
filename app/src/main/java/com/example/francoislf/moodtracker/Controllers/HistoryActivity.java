0840732e9239c32c38438e4a82db331d6ffb9e05
francoisLF <francois.lefort_pro@yahoo.com>
2018-03-20 09:24:56 +0100

    definition of xml layout of stick components for the HistoryActivity

--- app/src/main/java/com/example/francoislf/moodtracker/Controllers/HistoryActivity.java
+++ app/src/main/java/com/example/francoislf/moodtracker/Controllers/HistoryActivity.java
@@ -2,17 +2,20 @@ package com.example.francoislf.moodtracker.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.francoislf.moodtracker.R;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-
+        
 
 
     }
